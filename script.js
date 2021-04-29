7ff3767071cbe5e334ce343e5f072b36ad438105
smaxiso <44551068+smaxiso@users.noreply.github.com>
2021-04-29 15:20:50 +0530

    changes

--- script.js
+++ script.js
@@ -24,5 +24,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 1000); // Change image every 2 seconds
 }
